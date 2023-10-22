65e74cf6957104bad3f340c7186f8639ba11aec6
Mahabubur Rahman <mahabubmon@gmail.com>
2023-10-22 20:12:38 +0600

    event_listner

--- index.js
+++ index.js
@@ -2,3 +2,5 @@ const itemInput = document.getElementById("itemInput");
     const addItemButton = document.getElementById("addItemButton");
     const itemList = document.getElementById("itemList");
     const shoppingItems = [];
+// Add event listener to "Add Item" button
+addItemButton.addEventListener("click", addItemToList);
